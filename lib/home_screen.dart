c801615caf092f220d1402d7dbda952ae93ebe93
Dawn Surplus <>
2024-11-29 01:52:46 +0900

    [resolve #6] 시간 옵션 5가지로 확장

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,7 +10,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const int T_25_MIN_TO_SEC = 1500;
+  static const int T_15_MIN_TO_SEC = 60 * 15;
+  static const int T_15_MIN = 15;
+  static const int T_20_MIN_TO_SEC = 60 * 20;
+  static const int T_20_MIN = 20;
+  static const int T_25_MIN_TO_SEC = 60 * 25;
+  static const int T_25_MIN = 25;
+  static const int T_30_MIN_TO_SEC = 60 * 30;
+  static const int T_30_MIN = 30;
+  static const int T_35_MIN_TO_SEC = 60 * 35;
+  static const int T_35_MIN = 35;
 
   int totalSeconds = T_25_MIN_TO_SEC, totalPomodoros = 0;
   bool isRunning = false;
@@ -52,15 +61,38 @@ class _HomeScreenState extends State<HomeScreen> {
     return result;
   }
 
+  void setTime(int seconds) {
+    setState(() {
+      totalSeconds = seconds;
+    });
+  }
+
+  void setTime15min() {
+    setTime(T_15_MIN_TO_SEC);
+  }
+  void setTime20min() {
+    setTime(T_20_MIN_TO_SEC);
+  }
+  void setTime25min() {
+    setTime(T_25_MIN_TO_SEC);
+  }
+  void setTime30min() {
+    setTime(T_30_MIN_TO_SEC);
+  }
+  void setTime35min() {
+    setTime(T_35_MIN_TO_SEC);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,        
         children: [
           // Time
           Flexible(
-            flex: 1,
+            flex: 3,
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
@@ -69,6 +101,58 @@ class _HomeScreenState extends State<HomeScreen> {
                     color: Theme.of(context).cardColor,
                     fontSize: 89,
                     fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          // Timer Option
+          Flexible(
+            flex: 3,
+            child: SingleChildScrollView(
+              //scrollDirection: Axis.horizontal,
+              child: Row(              
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,                
+                children: [
+                  OutlinedButton (
+                      onPressed: setTime15min,
+                      style: OutlinedButton.styleFrom(backgroundColor: Theme.of(context).cardColor,
+                                                      side: const BorderSide(color: Colors.transparent)),
+                      child: Text("$T_15_MIN",
+                                  style: TextStyle(color: Theme.of(context).textTheme.displayLarge?.color,                                
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15),)),
+                  OutlinedButton (
+                      onPressed: setTime20min,
+                      style: OutlinedButton.styleFrom(backgroundColor: Theme.of(context).cardColor,
+                                                      side: const BorderSide(color: Colors.transparent)),
+                      child: Text("$T_20_MIN",
+                                  style: TextStyle(color: Theme.of(context).textTheme.displayLarge?.color,                                
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15),)),
+                  OutlinedButton (
+                      onPressed: setTime25min,
+                      style: OutlinedButton.styleFrom(backgroundColor: Theme.of(context).cardColor,
+                                                      side: const BorderSide(color: Colors.transparent)),
+                      child: Text("$T_25_MIN",
+                                  style: TextStyle(color: Theme.of(context).textTheme.displayLarge?.color,                                
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15),)),
+                  OutlinedButton (
+                      onPressed: setTime30min,
+                      style: OutlinedButton.styleFrom(backgroundColor: Theme.of(context).cardColor,
+                                                      side: const BorderSide(color: Colors.transparent)),
+                      child: Text("$T_30_MIN",
+                                  style: TextStyle(color: Theme.of(context).textTheme.displayLarge?.color,                                
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15),)),
+                  OutlinedButton (
+                      onPressed: setTime35min,
+                      style: OutlinedButton.styleFrom(backgroundColor: Theme.of(context).cardColor,
+                                                      side: const BorderSide(color: Colors.transparent)),
+                      child: Text("$T_35_MIN",
+                                  style: TextStyle(color: Theme.of(context).textTheme.displayLarge?.color,                                
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15),)),
+                ],
               ),
             ),
           ),
@@ -88,7 +172,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           // Count
           Flexible(
-            flex: 1,
+            flex: 2,
             child: Row(
               children: [
                 Expanded(
